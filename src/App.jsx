5df22c45b9fba6c76bfe599c843a48b7b02cd4d8
Adi Mehta <adimehta1970@gmail.co>
2023-06-15 22:52:24 +0530

    intial vite configuration

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable react/react-in-jsx-scope */
+import './App.css';
+
+function App() {
+
+	return (
+		<>
+		
+		</>
+	);
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable linebreak-style */
+// eslint-disable-next-line linebreak-style
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+);
